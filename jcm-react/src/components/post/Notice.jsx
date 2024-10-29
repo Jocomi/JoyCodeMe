1af3652dd7ca0f3ee981d2929122d390e01f3712
2week1 <julie1801@naver.com>
2024-10-29 16:05:53 +0900

    [2024-10-29 2weeks]
    1. MenuBar Width 95% -> 100%수정
    2. 고객지원 (PostPage) 수정 후 추가
    3. banner 변경
    4. 자유게시판/공지사항/프로젝트게시판/문의사항 jsx/css 추가

--- jcm-react/src/components/post/Notice.jsx
+++ jcm-react/src/components/post/Notice.jsx
@@ -1,16 +1,16 @@
-import '../../css/PostPage.css';
+import '../../css/Notice.css';
 import Pagination from 'react-bootstrap/Pagination';
 import Table from 'react-bootstrap/Table';
 import { NavLink } from 'react-router-dom';
 
-const PostPage = () => {
+const Notice = () => {
     return (
-        <div className="post-container">
-            <div className="post-banner">
+        <div className="project-container">
+            <div className="project-banner">
                 <img src="/img/banner.png" alt="Banner" />
-                <h1>고객지원</h1>
+                <h1>공지사항</h1>
             </div>
-            <div className="post-header">
+            <div className="project-header">
                 <ul>
                     <li>전체</li>
                     <li>작업</li>
@@ -26,7 +26,7 @@ const PostPage = () => {
                         <li><NavLink to="/Support/Questions">문의사항</NavLink></li>
                     </ul>
                 </div>
-            <div className="post-table">
+            <div className="project-table">
                 <Table hover responsive borderless>
                     <tbody>
                         <tr>
@@ -73,51 +73,8 @@ const PostPage = () => {
                     <Pagination.Last />
                 </Pagination>
             </div>
-
-
-        <div class="questions-div">
-            <h2 class="questions-left">자주 묻는 질문을 확인하세요!</h2>
-            <div class="questions-right">
-                <ol>
-                <li class="question-item">LEVEL마다 제공되는게 뭐가 다른지 정확히 알고 싶어요.
-                    <div class="answer">
-                    각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
-                    </div>
-                </li>
-                <li class="question-item">미리보기 부분이 나오지 않아요.
-                    <div class="answer">
-                    각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
-                    </div>
-                </li>
-                <li class="question-item">게시판에 글이 올라가지 않는데 왜그런가요?
-                    <div class="answer">
-                    각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
-                    </div>
-                </li>
-                <li class="question-item">사용법을 잘 모르겠어요.
-                    <div class="answer">
-                    각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
-                    </div>
-                </li>
-                <li class="question-item">기능사용 횟수에 제한이 있나요?
-                    <div class="answer">
-                    각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
-                    </div>
-                </li>
-                <li class="question-item">결제를 했는데 기능 사용이 안돼요.
-                    <div class="answer">
-                    각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
-                    </div>
-                </li>
-                <li class="question-item">환불을 하고 싶은데 환불은 어떻게 하나요?
-                    <div class="answer">
-                    각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
-                    </div>
-                </li>
-                </ol>
-            </div>
         </div>
-    </div>
     );
-}
-export default PostPage;
+};
+
+export default Notice;
