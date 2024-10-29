71bb4e2e45df0dea7eff5fb7ad2ce8078ceb5e9d
2week1 <julie1801@naver.com>
2024-10-29 16:26:53 +0900

    [2024-10-29] 2weeks
    FrequentlyQuestions (자주 묻는 질문) 링크로 분리

---
+++ jcm-react/src/components/post/FrequentlyQuestions.jsx
@@ -0,0 +1,52 @@
+import '../../css/FrequentlyQuestions.css';
+
+const FrequentlyQuestions = () => {
+    return (
+
+        <div class="questions-div">
+            <h2 class="questions-left">자주 묻는 질문을 확인하세요!</h2>
+            <div class="questions-right">
+                <ol>
+                    <li className="question-item">LEVEL마다 제공되는게 뭐가 다른지 정확히 알고 싶어요.
+                        <div className="answer">
+                            각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
+                        </div>
+                    </li>
+                    <li className="question-item">미리보기 부분이 나오지 않아요.
+                        <div className="answer">
+                            각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
+                        </div>
+                    </li>
+                    <li className="question-item">게시판에 글이 올라가지 않는데 왜그런가요?
+                        <div className="answer">
+                            각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
+                        </div>
+                    </li>
+                    <li className="question-item">사용법을 잘 모르겠어요.
+                        <div className="answer">
+                            각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
+                        </div>
+                    </li>
+                    <li className="question-item">기능사용 횟수에 제한이 있나요?
+                        <div className="answer">
+                            각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
+                        </div>
+                    </li>
+                    <li className="question-item">결제를 했는데 기능 사용이 안돼요.
+                        <div className="answer">
+                            각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
+                        </div>
+                    </li>
+                    <li className="question-item">환불을 하고 싶은데 환불은 어떻게 하나요?
+                        <div className="answer">
+                            각 LEVEL에 따라 제공되는 기능과 혜택이 다릅니다. 자세한 정보는 고객센터에 문의하세요.
+                        </div>
+                    </li>
+                </ol>
+            </div>
+        </div>
+
+    );
+};
+
+export default FrequentlyQuestions;
