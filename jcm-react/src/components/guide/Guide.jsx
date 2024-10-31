375af74abb53277bc15aca0ad9538e515a1a2cf8
2week1 <julie1801@naver.com>
2024-10-31 12:27:53 +0900

    [2024-10-31] 2weeks
    1. 가이드 페이지 UI 추가
    2. 가이드 페이지 React 적용

--- jcm-react/src/components/guide/Guide.jsx
+++ jcm-react/src/components/guide/Guide.jsx
@@ -1,10 +1,145 @@
-import {Link} from 'react-router-dom';
-import '../../css/guide/Guide.css'
+import '../../css/guide/Guide.css';
 
 const Guide = () => {
-    return(
+    return (
         <div className='guide-container'>
-            테스트테스트원투쓰리!
+            <div className="row">
+                <div className="col-md-12 mb-5">
+                    <h2 className="main-head">Transparent and flexible pricing</h2>
+                    <p className="sub-head">Design and code in a visual, collaborative way, export for free</p>
+                </div>
+
+                {/* <!-- Purple Table --> */}
+                <div className="col-md-4">
+                    <div className="pricing-table purple">
+                        {/* <!-- Table Head --> */}
+                        <div className="pricing-label">Fixed Price</div>
+                        <h2>Beginner Pack 2024</h2>
+                        <h5>Made for starters</h5>
+
+                        {/* <!-- Features --> */}
+                        <div className="pricing-features">
+                            <div className="feature">Bandwith<span>50 GB</span></div>
+                            <div className="feature">Add-On Domains<span>10</span></div>
+                            <div className="feature">SSD Storage<span>250 GB</span></div>
+                            <div className="feature">Mail Adresses<span>25</span></div>
+                            <div className="feature">SEO Optimization<span>Basic</span></div>
+                            <div className="feature">Customizable Design Templates<span>None</span></div>
+                            <div className="feature">Site Backup<span>Montly</span></div>
+                        </div>
+                        {/* <!-- Price --> */}
+                        <div className="price-tag">
+                            <span className="symbol">$</span>
+                            <span className="amount">7.99</span>
+                            <span className="after">/month</span>
+                        </div>
+
+                        {/* <!-- Button --> */}
+                        <a className="price-button" href="#">Get Started</a>
+                    </div>
+                </div>
+
+                {/* <!-- Turquoise Table --> */}
+                <div className="col-md-4">
+                    <div className="pricing-table turquoise">
+
+                        {/* <!-- Table Head --> */}
+                        <div className="pricing-label">Fixed Price</div>
+                        <h2>Intermediate Pack 2024</h2>
+                        <h5>Made for experienced users</h5>
+
+                        {/* <!-- Features --> */}
+                        <div className="pricing-features">
+                            <div className="feature">Bandwith<span>150 GB</span></div>
+                            <div className="feature">Add-On Domains<span>25</span></div>
+                            <div className="feature">SSD Storage<span>500 GB</span></div>
+                            <div className="feature">Mail Adresses<span>50</span></div>
+                            <div className="feature">SEO Optimization<span>Enhanced</span></div>
+                            <div className="feature">Customizable Design Templates<span>Support</span></div>
+                            <div className="feature">Site Backup<span>Weekly</span></div>
+                        </div>
+
+                        {/* <!-- Price --> */}
+                        <div className="price-tag">
+                            <span className="symbol">$</span>
+                            <span className="amount">9.99</span>
+                            <span className="after">/month</span>
+                        </div>
+
+                        {/* <!-- Button --> */}
+                        <a className="price-button" href="#">Get Started</a>
+                    </div>
+                </div>
+
+                {/* <!-- Red Table --> */}
+                <div className="col-md-4">
+                    <div className="pricing-table red">
+
+                        {/* <!-- Table Head --> */}
+                        <div className="pricing-label">Fixed Price</div>
+                        <h2>Advanced Pack 2024</h2>
+                        <h5>Made for professionals/agencies users</h5>
+
+                        {/* <!-- Features --> */}
+                        <div className="pricing-features">
+                            <div className="feature">Bandwith<span>250 GB</span></div>
+                            <div className="feature">Add-On Domains<span>50</span></div>
+                            <div className="feature">SSD Storage<span>1 TB</span></div>
+                            <div className="feature">Mail Adresses<span>75</span></div>
+                            <div className="feature">SEO Optimization<span>Advanced</span></div>
+                            <div className="feature">Customizable Design Templates<span>Support</span></div>
+                            <div className="feature">Site Backup<span>Automated Daily</span></div>
+
+                        </div>
+                        {/* <!-- Price --> */}
+                        <div className="price-tag">
+                            <span className="symbol">$</span>
+                            <span className="amount">12.99</span>
+                            <span className="after">/month</span>
+                        </div>
+                        {/* <!-- Button --> */}
+                        <a className="price-button" href="#">Get Started</a>
+                    </div>
+                </div>
+                {/* <!-- All plans Include --> */}
+                <div className="col-md-12 mt-5">
+                    <h3 className="section-head">All plans include</h3>
+                    <div className="all-plans-features">
+                        <div className="feature">
+                            <h4>Free domain hosting</h4>
+                            <p>Host your project for free on a subdomain. You can always  move to domains.</p>
+                        </div>
+                        <div className="feature">
+                            <h4>Google Fonts</h4>
+                            <p>Access a wide selection of Google Web Fonts without needing to install anything.</p>
+                        </div>
+                        <div className="feature">
+                            <h4>Interactions and states</h4>
+                            <p>Visual indicators for component states and interactive elements.</p>
+                        </div>
+                        <div className="feature">
+                            <h4>Responsive templates</h4>
+                            <p>Access a library of responsive static templates, eCommerce storefronts, and ready-to-use components.</p>
+                        </div>
+                        <div className="feature">
+                            <h4>Global styleguide</h4>
+                            <p>Organize and manage your design system colors, components, fonts to maintain brand consistency.</p>
+                        </div>
+                        <div className="feature">
+                            <h4>Use custom fonts</h4>
+                            <p>Import preferred fonts and use them in projects. Fonts are saved in your library.</p>
+                        </div>
+                        <div className="feature">
+                            <h4>Smart Media Queries</h4>
+                            <p>Simplify responsive design based on device characteristics and parameters.</p>
+                        </div>
+                        <div className="feature">
+                            <h4>Figma Import</h4>
+                            <p>Import prototypes into the platform, with developer-friendly code for HTML, CSS, or frameworks like React.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
