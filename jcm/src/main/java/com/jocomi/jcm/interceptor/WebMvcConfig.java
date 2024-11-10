f45cef6acde091a3c4a50aee490304868fd784f2
KDW <rlekdns@gmail.com>
2024-11-10 19:39:42 +0900

    [2024-11-10] Download / CORS 통신 기능 추가 / 외부접속시 통신오류 해결 CORS 통신 기능 추가 외부에서 접속시 localhost:3000 으로 접속되면서 DB 연동 문제발생한점 전역IP 로 해결 완료 + ( 추후 다른 코드들도 수정 예정 )

--- jcm/src/main/java/com/jocomi/jcm/interceptor/WebMvcConfig.java
+++ jcm/src/main/java/com/jocomi/jcm/interceptor/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.jocomi.jcm.interceptor;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     public WebMvcConfig(LoginInterceptor loginInterceptor) {
         this.loginInterceptor = loginInterceptor;
+    }
+    
+ 
+    public void addCorsMapping(CorsRegistry registry) {
+    	registry.addMapping("/**")
+    		.allowedOrigins("*")
+    		.allowedMethods("GET","POST","PUT","DELETE")
+    		.allowedHeaders("*")
+    		.allowCredentials(false);
     }
 
     @Override
