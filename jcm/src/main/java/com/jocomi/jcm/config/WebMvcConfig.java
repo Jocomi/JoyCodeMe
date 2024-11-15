98ea3250d2ca582e3a97712df6c9268ffc96fceb
yangjun565 <ppp32070@naver.com>
2024-11-15 17:46:51 +0900

    [2024.11.15] Zeus : 충돌났던 파일 있어서 충돌 해결 후 재커밋합니다. DB 다시 돌려야 게시판 잘 작동합니다.

--- jcm/src/main/java/com/jocomi/jcm/config/WebMvcConfig.java
+++ jcm/src/main/java/com/jocomi/jcm/config/WebMvcConfig.java
@@ -2,7 +2,7 @@ package com.jocomi.jcm.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,5 +16,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
             .allowedHeaders("*")
             .allowCredentials(false);
     }
-
+    @Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/uploads/**")
+				.addResourceLocations("file:./uploads/");
+	}
 }
