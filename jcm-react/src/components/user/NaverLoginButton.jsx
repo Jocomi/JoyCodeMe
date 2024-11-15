15a5263f9822198a961d3e3ff393c3bbb066f85a
2week1 <julie1801@naver.com>
2024-11-15 16:23:48 +0900

    [24-11-15] 2week 네이버/구글 로그인 버튼 css 추가수정

--- jcm-react/src/components/user/NaverLoginButton.jsx
+++ jcm-react/src/components/user/NaverLoginButton.jsx
@@ -1,11 +1,8 @@
-// src/components/NaverLoginButton.js
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 const NaverLoginButton = () => {
 
     useEffect(() => {
-        // 네이버 SDK 로드
         const script = document.createElement("script");
         script.src = "https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.2.js";
         script.type = "text/javascript";
@@ -14,22 +11,25 @@ const NaverLoginButton = () => {
         script.onload = () => {
             const naverLogin = new window.naver.LoginWithNaverId({
                 clientId: process.env.REACT_APP_NAVER_CLIENT_ID,
-                callbackUrl: "http://localhost:3000/naver/callback", // 프론트엔드 콜백 URL로 변경
+                callbackUrl: "http://localhost:3000/naver/callback",
                 isPopup: false,
-                loginButton: { color: "green", type: 4, height: 40 },
+                loginButton: { color: "green", type: 2, height: 40 },
                 callbackHandle: false,
             });
 
             naverLogin.init();
         };
 
-        // Cleanup script on unmount
         return () => {
             document.head.removeChild(script);
         };
     }, []);
 
-    return <div id="naverIdLogin" />; // 네이버 로그인 버튼 표시
+    return (
+        <div className="naver-login-container">
+            <div id="naverIdLogin" />
+        </div>
+    );
 };
 
 export default NaverLoginButton;
