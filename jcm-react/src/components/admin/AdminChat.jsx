7142b05f960a7948ee65eb420610b0b46f5af2da
2week1 <julie1801@naver.com>
2024-11-18 10:51:59 +0900

    [24-11-18] 2week AdminChat 페이지 다시 추가

--- jcm-react/src/components/admin/AdminChat.jsx
+++ jcm-react/src/components/admin/AdminChat.jsx
@@ -1,23 +1,28 @@
 import AdminSideBar from "./AdminSideBar";
 import '../../css/admin/Chat.css';
 
+const AdminChat = () => {
+    const handleButtonClick = () => {
+        window.open("https://desk.channel.io/#/channels/183715/team_chats/groups/417412", "_blank");
+    };
 
-const AdminChat = () =>{
-    return(
-    <>
-        <div className="chat-main">
-            <AdminSideBar/>
-            <div className="main-content">
-                <h1>Chat</h1>
-                <hr />
-                <section className="chat">
-                    <h2>Chat with Customers</h2>
-                    <p>No new messages at the moment.</p>
-                </section>
+    return (
+        <>
+            <div className="chat-main">
+                <AdminSideBar />
+                <div className="main-content">
+                    <h1>Chat</h1>
+                    <hr />
+                    <section className="chat">
+                        <h2>채팅 관리</h2>
+                        <button className="chat-button" onClick={handleButtonClick}>
+                            채팅 관리 페이지
+                        </button>
+                    </section>
+                </div>
             </div>
-        </div>
-    </>
-    )
-}
+        </>
+    );
+};
 
 export default AdminChat;
