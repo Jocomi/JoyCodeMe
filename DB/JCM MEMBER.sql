ba0225a37be304afa2292d00522537689c8c8138
KDW0401 <rlekdns@gmail.com>
2024-11-21 11:23:33 +0900

    [2024-11-21] Download / Member 쿼리문 수정후 재업로드 가입일 데이터 추가

--- DB/JCM MEMBER.sql
+++ DB/JCM MEMBER.sql
@@ -1,18 +1,17 @@
 INSERT INTO MEMBER
-VALUES ('Rocomi', '1234', '엄희윤', 'rocomi@gmail.com', '010-1xxx-xxxx', TO_DATE('2000-01-01', 'YYYY-MM-DD'), '역삼', 'A' ,NULL);
+VALUES ('Rocomi', '1234', '엄희윤', 'rocomi@gmail.com', '010-1xxx-xxxx', TO_DATE('2000-01-01', 'YYYY-MM-DD'), '역삼', 'A' , NULL , TO_DATE('2024-09-01', 'YYYY-MM-DD'));
 
 INSERT INTO MEMBER
-VALUES ('Download', '1234', '기다운', 'download@gmail.com', '010-2xxx-xxxx', TO_DATE('2000-01-01', 'YYYY-MM-DD'), '구로', 'A', NULL);
+VALUES ('Download', '1234', '기다운', 'download@gmail.com', '010-2xxx-xxxx', TO_DATE('2000-01-01', 'YYYY-MM-DD'), '구로', 'A', NULL, TO_DATE('2024-09-02', 'YYYY-MM-DD'));
 
 INSERT INTO MEMBER
-VALUES ('2weeks', '1234', '이주원', '2weeks@gmail.com', '010-3xxx-xxxx', TO_DATE('2000-01-01', 'YYYY-MM-DD'), '평택', 'A', NULL);
+VALUES ('2weeks', '1234', '이주원', '2weeks@gmail.com', '010-3xxx-xxxx', TO_DATE('2000-01-01', 'YYYY-MM-DD'), '평택', 'A', NULL, TO_DATE('2024-10-01', 'YYYY-MM-DD'));
 
 INSERT INTO MEMBER
-VALUES ('Zeus', '1234', '양준혁', 'zeus@gmail.com', '010-4xxx-xxxx', TO_DATE('2000-01-01', 'YYYY-MM-DD'), '평택', 'A', NULL);
+VALUES ('Zeus', '1234', '양준혁', 'zeus@gmail.com', '010-4xxx-xxxx', TO_DATE('2000-01-01', 'YYYY-MM-DD'), '평택', 'A', NULL, TO_DATE('2024-10-05', 'YYYY-MM-DD'));
 
 INSERT INTO MEMBER
-VALUES ('test1', '1234', '테스트', 'rocomi1@gmail.com', '010-12xx-xxxx', TO_DATE('2000-01-01', 'YYYY-MM-DD'), '역삼', 'Y', NULL);
-
+VALUES ('test1', '1234', '테스트', 'rocomi1@gmail.com', '010-12xx-xxxx', TO_DATE('2000-01-01', 'YYYY-MM-DD'), '역삼', 'Y', NULL, TO_DATE('2024-11-20', 'YYYY-MM-DD'));
     
 COMMIT;
 
